87bf7f2048e9f1bbeaf582e2a281017fcc7b686c
BartolMarko <bartolmarkovinovic@gmail.com>
2026-02-21 12:18:11 +0100

    add downloading of agents to factory

--- src/agent_factory.py
+++ src/agent_factory.py
@@ -1,13 +1,53 @@
 import torch
 from hockey.hockey_env import HockeyEnv
+import os
 
 from src.named_agent import NamedAgent, WeakBot, StrongBot, SACLastYearAgent
 from src.TDMPC.agent import TDMPCAgent
 from src.TD3.td3 import TD3
 from src.TD3.config_reader import Config
 from src.SAC.helper import get_my_sac
+from src import wandb_utils
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+MODEL_SAVE_FOLDER = "models"
+
+
+def download_agent_from_wandb(
+    agent_name: str, agent_cfg: dict, save_folder: str = MODEL_SAVE_FOLDER
+) -> None:
+    """Downloads agent from wandb if wandb information is provided in the agent configuration."""
+    if "wandb_run_id" in agent_cfg and "wandb_folder" in agent_cfg:
+        wandb_utils.download_wandb_folder(
+            run_id=agent_cfg["wandb_run_id"],
+            wandb_folder=agent_cfg["wandb_folder"],
+            destination_folder=os.path.join(save_folder, agent_name),
+        )
+
+        model_folder = os.path.join(save_folder, agent_name, agent_cfg["wandb_folder"])
+        agent_cfg["load_dir"] = model_folder
+        agent_cfg["weights_path"] = os.path.join(model_folder, "model.pt")
+        agent_cfg["config_path"] = os.path.join(model_folder, "config.yaml")
+        print(
+            f"Downloaded agent: {agent_name} from run:",
+            f"{agent_cfg['wandb_run_id']} folder: {agent_cfg['wandb_folder']}",
+        )
+
+    elif "wandb_artifact" in agent_cfg and agent_cfg["wandb_artifact"] is not None:
+        artifact_info = agent_cfg["wandb_artifact"]
+        wandb_utils.download_wandb_artifact(
+            artifact_path=artifact_info["artifact_path"],
+            artifact_version=artifact_info["artifact_version"],
+            destination_folder=os.path.join(save_folder, agent_name),
+        )
+
+        model_folder = os.path.join(save_folder, agent_name)
+        agent_cfg["weights_folder"] = model_folder
+        print(
+            f"Downloaded agent: {agent_name} from artifact:",
+            f"{artifact_info['artifact_path']}:{artifact_info['artifact_version']}",
+        )
 
 
 def create_td3_agent(name: str, weights_path: str, config_path: str) -> TD3:
@@ -27,6 +67,7 @@ def create_td3_agent(name: str, weights_path: str, config_path: str) -> TD3:
 
 def agent_factory(agent_name: str, agent_cfg: dict) -> NamedAgent:
     """Factory function to create agents based on the configuration."""
+    download_agent_from_wandb(agent_name, agent_cfg)
     match agent_cfg["type"]:
         case "TD3":
             return create_td3_agent(
@@ -46,8 +87,7 @@ def agent_factory(agent_name: str, agent_cfg: dict) -> NamedAgent:
             return tdmpc_agent
         case "SAC":
             return get_my_sac(
-                cfg_path=agent_cfg["config_path"],
-                w_folder=agent_cfg["weights_folder"]
+                cfg_path=agent_cfg["config_path"], w_folder=agent_cfg["weights_folder"]
             )
         case "SACLastYear":
             return SACLastYearAgent(env=HockeyEnv())
@@ -63,11 +103,13 @@ def test_sac_agent_factory():
     """Test function for the SAC agent factory."""
     config_yaml_path = "src/SAC/sac_example.yaml"
     import yaml
-    with open(config_yaml_path, 'r') as file:
+
+    with open(config_yaml_path, "r") as file:
         sac_config = yaml.safe_load(file)
     sac_agent = agent_factory("TestSACAgent", sac_config)
-    assert isinstance(sac_agent, NamedAgent), \
+    assert isinstance(sac_agent, NamedAgent), (
         "The created agent should be an instance of NamedAgent."
+    )
     print("SAC agent factory test passed.")
 
 

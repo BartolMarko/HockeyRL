1259d32fd6cd8ba01852560ff4e87f307a9dfa43
BartolMarko <bartolmarkovinovic@gmail.com>
2026-02-25 00:48:14 +0100

    add launch to if else bot

--- src/TD3/if_else_bot.py
+++ src/TD3/if_else_bot.py
@@ -1,15 +1,16 @@
 import torch
-import uuid
 import hockey.hockey_env as h_env
 
 from src.TD3.td3 import TD3
 from src.TD3.config_reader import Config
 from src.named_agent import NamedAgent
-
+from comprl.client import launch_client
 
 ATTACK_AGENT_PATH = "./models/td3/177k/checkpoint_step_177000_model.pt"
 DEFENSE_AGENT_PATH = "./models/td3/defender/checkpoint_step_8000_model.pt"
 CONFIG_PATH = "./models/td3/defender/checkpoint_step_8000_config.yaml"
+
+
 class IfElseBot(NamedAgent):
     def __init__(self):
         super().__init__("IfElseBot")
@@ -19,7 +20,7 @@ class IfElseBot(NamedAgent):
 
         self.attack_agent = TD3(cfg)
         self.defend_agent = TD3(cfg)
-        
+
         self.attack_agent.restore_state(torch.load(ATTACK_AGENT_PATH))
         self.defend_agent.restore_state(torch.load(DEFENSE_AGENT_PATH))
 
@@ -32,9 +33,9 @@ class IfElseBot(NamedAgent):
             self.init_step = True
         self_has_puck = obs[16] > 0
         puck_x_pos = obs[12]
-        if (puck_x_pos < 0 and self.init_step):
+        if puck_x_pos < 0 and self.init_step:
             self.done = True
-        if ((not self.done) and self_has_puck):
+        if (not self.done) and self_has_puck:
             self.done = True
         self.init_step = False
         if not self.done:
@@ -43,7 +44,7 @@ class IfElseBot(NamedAgent):
 
     def act(self, obs):
         return self.get_step(obs)
-    
+
     @staticmethod
     def _is_round_start(obs):
         x_pos_p1 = obs[0]
@@ -53,15 +54,21 @@ class IfElseBot(NamedAgent):
         puck_vel_x = obs[14]
         puck_vel_y = obs[15]
 
-        return (x_pos_p1 == -3.0 and 
-                x_pos_p2 ==  3.0 and 
-                theta_p1 == 0.0 and
-                theta_p2 == 0.0 and
-                puck_vel_x == 0.0 and
-                puck_vel_y == 0.0)
-    
+        return (
+            x_pos_p1 == -3.0
+            and x_pos_p2 == 3.0
+            and theta_p1 == 0.0
+            and theta_p2 == 0.0
+            and puck_vel_x == 0.0
+            and puck_vel_y == 0.0
+        )
+
     def on_start_game(self, game_id) -> None:
         pass
-    
+
     def on_end_game(self, result, stats):
         pass
+
+
+if __name__ == "__main__":
+    launch_client(lambda _: IfElseBot())
